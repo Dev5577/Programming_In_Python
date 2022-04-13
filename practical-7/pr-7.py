36136de0a34ed61720cc1645182547ea23201cf9
Dev5577 <91939807+Dev5577@users.noreply.github.com>
2022-04-13 13:46:31 +0530

    Add files via upload

---
+++ practical-7/pr-7.py
@@ -0,0 +1,15 @@
+# Name: Sunva Divyaraj
+# ID:  20CS089
+
+Text = int(input())
+for i in range(Text):
+ n = input()
+ l = len(n)
+ if l % 2 == 0:
+    b, c = n[:l//2], n[l//2:]
+ else:
+    b, c = n[:l//2], n[l//2+1:]
+ if sorted(b) == sorted(c):
+    print("YES")
+ else:
+    print("NO")
